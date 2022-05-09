893963e778cd5960fbbbb3fbf39b01eddfb69d98
NoobMahbub <Mahbubbengali@gmail.com>
2022-05-10 00:25:34 +0600

    done

---
+++ phonehunter.js
@@ -0,0 +1,9 @@
+const submit = document.getElementById("submit");
+
+submit.addEventListener("click",()=>{
+const phoneInput = document.getElementById('phoneInput');
+const phoneName = phoneInput.value;
+console.log(phoneName);
+
+});
+
